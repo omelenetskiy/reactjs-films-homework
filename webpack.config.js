6da918c0a2c26d3ebb8e099ce2b76df3cf969300
Pavel Dubina <pavelek_name@mail.com>
2020-12-14 18:46:34 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2020 EPAM Systems, Inc. All Rights Reserved. All information contained herein is, and remains the
+ * property of EPAM Systems, Inc. and/or its suppliers and is protected by international intellectual
+ * property law. Dissemination of this information or reproduction of this material is strictly forbidden,
+ * unless prior written permission is obtained from EPAM Systems, Inc
+ */
+
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
+module.exports = {
+  entry: [
+    "webpack-hot-middleware/client",
+    'react-hot-loader/patch',
+    "./src/index.js",
+  ],
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name].js",
+    publicPath: '/',
+  },
+  plugins: [htmlPlugin, new webpack.HotModuleReplacementPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: "file-loader",
+        options: { name: "/assets/images/[name].[ext]" },
+      },
+    ],
+  },
+};
